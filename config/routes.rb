0b87ccc7e139289c84c4825e229713a2940bb6ac
Peter Wang <happypeter1983@gmail.com>
2011-12-20 19:55:52 +0800

    login  works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Ccc::Application.routes.draw do
   
   resources :users
+  resources :sessions
   get "log_in" => "sessions#new", :as => "log_in" 
   get "log_out" => "sessions#destroy", :as => "log_out"  
   get "sign_up" => "users#new", :as => "sign_up"  
