2fd264618c60e1b8766e33d6159f725dc9f24b03
Peter Wang <happypeter1983@gmail.com>
2011-12-20 15:50:47 +0800

    add user by hand

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController  
+  def new  
+    @user = User.new  
+  end
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to(@user, :notice => 'Profile was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+      end
+    end
+  end  
+    
+  def create  
+    @user = User.new(params[:user])  
+    if @user.save  
+      session[:user_id] = @user.id 
+      redirect_to root_url, :notice => "signed up!"  
+    else  
+      render "new"  
+    end  
+  end  
+
+  def index
+      @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def show
+    if params[:name]
+      @user = User.where(:name => params[:name]).first
+    else
+      @user = User.find(params[:id])
+    end
+
+    if @user == nil
+      redirect_to root_url, :notice => "no such user!"  
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+      end
+    end
+  end
+end
+
