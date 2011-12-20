6282dce5becee9ef7beb34b59da13c88f4e16fab
Peter Wang <happypeter1983@gmail.com>
2011-12-20 16:03:40 +0800

    trying to do everything by hand

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,6 @@ class UsersController < ApplicationController
   end  
 
   def index
-      @users = User.all
-
     respond_to do |format|
       format.html # index.html.erb
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Ccc::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  
+  resources :users
 
   # Sample resource route with options:
   #   resources :products do
