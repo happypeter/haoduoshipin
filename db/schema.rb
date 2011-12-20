ff9e4dad60bcfab405a93606d93bce5aec6b523c
Peter Wang <happypeter1983@gmail.com>
2011-12-20 18:20:39 +0800

    r g migration AddNameToUsers name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20111220101805) do
+
+  create_table "users", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
