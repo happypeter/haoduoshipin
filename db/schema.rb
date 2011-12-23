ab3cf2d1ff06924d7eafc1eba836745b0edbc5e7
Peter Wang <happypeter1983@gmail.com>
2011-12-23 15:05:12 +0800

    works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111221091714) do
+ActiveRecord::Schema.define(:version => 20111223051006) do
 
   create_table "episodes", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(:version => 20111221091714) do
     t.datetime "updated_at"
     t.integer  "position"
     t.text     "notes"
+    t.datetime "published_at"
+    t.integer  "seconds"
+    t.text     "description"
   end
 
   create_table "users", :force => true do |t|
