795e1ce29a3dd8e01e6047a8b9a9745e84e12963
Peter Wang <happypeter1983@gmail.com>
2011-12-23 11:15:31 +0800

    cancan works

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,7 @@
 class Ability
   include CanCan::Ability
-  user ||= User.new
 
   def initialize(user)
-    if user # the user is just the current_user, if you are not logged in, the user will be nil 
-      if user.id
-      end
-    end
     can :read, :episodes
-    can :destory, :all
   end
 end
