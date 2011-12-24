a25328ebd474c0c06e199190d4d05aaaa3742430
Peter Wang <happypeter1983@gmail.com>
2011-12-24 12:54:10 +0800

    a icon need to mean sth, so I remove the dog
    and use a screen from openclipart.org instead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def markdown(text)
-    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
+    options = [:hard_wrap, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
     syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
   end
 
