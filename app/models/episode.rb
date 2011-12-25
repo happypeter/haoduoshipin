6148883c7318b41206a1afdf9cb974871d6c6f3f
Peter Wang <happypeter1983@gmail.com>
2011-12-25 17:22:55 +0800

    file_size not working

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -11,5 +11,16 @@ class Episode < ActiveRecord::Base
   def permalink
     self.name #RC has sth smarter, will use later
   end
+  def files
+    [
+      {:name => "mp4",         :info => "Full Size H.264 Video",  :url => asset_url("videos", "mp4"),  :size => 1},
+      {:name => "mov",         :info => "Smaller H.264 Video",    :url => asset_url("videos", "mov"),  :size => 1},
+    ]
+  end
+
+  # TODO test me
+  def available_files
+    files.select { |f| f[:size].to_i > 0 }
+  end
 
 end
