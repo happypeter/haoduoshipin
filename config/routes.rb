73c8333fbcacb7396d1fa5abb58d953deda12381
Peter Wang <happypeter1983@gmail.com>
2011-12-25 11:34:16 +0800

    about

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Ccc::Application.routes.draw do
   resources :users
   resources :episodes
   resources :sessions
+  match "about" => "info#about", :as => "about"
   get "log_in" => "sessions#new", :as => "log_in" 
   get "log_out" => "sessions#destroy", :as => "log_out"  
   get "sign_up" => "users#new", :as => "sign_up"  
