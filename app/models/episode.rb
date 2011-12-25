436ecea7aef076ed306be5c3cecd20a5ecef660f
Peter Wang <happypeter1983@gmail.com>
2011-12-25 16:33:47 +0800

    001-ctags.mov

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,10 +3,10 @@ class Episode < ActiveRecord::Base
     [padded_position, permalink].join('-')
   end
   def asset_url(path, ext = nil)
-    "http://media.railscasts.com/assets/episodes/#{path}/#{asset_name}" + (ext ? ".#{ext}" : "")
+    "http://media.happypeter.org/happycasts/episodes/#{path}/#{asset_name}" + (ext ? ".#{ext}" : "")
   end
   def padded_position
-    position.to_s.rjust(3, "0")
+    position.to_s.rjust(3, "0") # so we need file name "001-xxx.mov" not "1-xxx.mov"
   end
   def permalink
     "hello"
