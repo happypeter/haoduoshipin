a7fbe70ad338f6ccff43083b39b5a1a19c10542a
Peter Wang <happypeter1983@gmail.com>
2011-12-26 13:55:20 +0800

    cp code from RC for comment

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+class Comment < ActiveRecord::Base
+  belongs_to :episode, :counter_cache => true
+  belongs_to :user
+
+  validates_presence_of :content, :episode_id
+
+  scope :recent, order("created_at DESC")
+
+  has_ancestry
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :password  
   before_save :encrypt_password  
+  has_many :comments
 
   validates_confirmation_of :password  
   validates_presence_of :password, :on => :create
