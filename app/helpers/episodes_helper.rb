9ce2395020a6e1065049328836e927ad1e67a74a
Peter Wang <happypeter1983@gmail.com>
2011-12-28 11:33:09 +0800

    768x576

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -1,5 +1,5 @@
 module EpisodesHelper
   def episode_video_tag(episode)
-    video_tag episode.asset_url("videos"), :poster => "http://media.happypeter.org/happycasts/episodes/stills/load.png", :width => 1024, :height =>768 
+    video_tag episode.asset_url("videos"), :poster => "http://media.happypeter.org/happycasts/episodes/stills/load.png", :width => 768, :height =>576 
   end
 end
