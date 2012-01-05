44ff6f521be96552f321db4d4b6040a26b6ade04
Peter Wang <happypeter1983@gmail.com>
2012-01-05 14:42:09 +0800

    about

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,11 @@ class Ability
     can :read, :episodes, ["published_at <= ?", Time.zone.now] do |episode|
       episode.published_at <= Time.now.utc
     end
+    can :access, :info # everybody can visit the about page
+    can [:read, :create, :log_in], :users
     if user # when sb logged in
+      can :log_out, :users
+      can :update, :users, :id => user.id
       if user.name == "happypeter"
         can :access, :all
       end
