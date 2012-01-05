59298e70a4fd8a8c1aa3daf38702e8340916211a
Peter Wang <happypeter1983@gmail.com>
2012-01-05 14:02:09 +0800

    now only happypeter can see the unpublished

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, :episodes
+    can :read, :episodes, ["published_at <= ?", Time.zone.now] do |episode|
+      episode.published_at <= Time.now.utc
+    end
     if user # when sb logged in
       if user.name == "happypeter"
         can :access, :all
