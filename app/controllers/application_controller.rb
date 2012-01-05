6891b398193f6bc68394dee95a988f12bb954b64
Peter Wang <happypeter1983@gmail.com>
2012-01-05 14:33:31 +0800

    handle cancan accessdenied event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,8 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+  end
+  enable_authorization do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
 end
