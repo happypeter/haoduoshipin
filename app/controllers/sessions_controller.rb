15d396f321416063f05f78ed2c07f3d5d7502498
Peter Wang <happypeter1983@gmail.com>
2012-01-06 14:34:40 +0800

    i

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       redirect_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid name or password"
-      render "new"
+      redirect_to :action => "new" 
+      # render "new", won't have a new @user instance, which result in failing to render new.html.erb
     end
   end
 
