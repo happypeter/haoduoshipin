611cbc3aab4a134c65064ccd7dc6ecec2ff7a15e
Peter Wang <happypeter1983@gmail.com>
2012-04-16 10:09:04 +0800

    simpler code

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,11 +1,7 @@
 class EpisodesController < ApplicationController
   load_and_authorize_resource 
   def index
-    if params[:tag_id]  
-      if params[:tag_id].to_i != 0
-        @tag = Tag.find(params[:tag_id]) 
-      end
-    end
+    @tag = Tag.find(params[:tag_id]) if params[:tag_id]  
     if params[:search].blank?
       @episodes = (@tag ? @tag.episodes : Episode).accessible_by(current_ability).recent
     else
