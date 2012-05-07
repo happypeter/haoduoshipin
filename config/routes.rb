2bb3ae96e1ac726d9ddd66df39440fd9f1ace605
Peter Wang <happypeter1983@gmail.com>
2012-05-07 15:55:51 +0800

    i

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Happycasts::Application.routes.draw do
   match "/auth/:provider/callback" => "users#login_with_github"
   match "/auth/failure" => "users#login_with_github_failure"
   match "/user_login" =>"users#login" # when user try to login with their local account
-  match "about" => "info#about", :as => "about"
+  match "/about" => "info#about", :as => "about"
   get "log_in" => "users#new", :as => "log_in"  
   get "log_out" => "users#logout", :as => "log_out"  
 
