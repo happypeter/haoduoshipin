183a6f72ccbe3891a9b48f0611f276527a4088c0
Peter Wang <happypeter1983@gmail.com>
2012-06-13 16:24:00 +0800

    everybody can view comments#index as it should

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,12 @@ class Ability
     can :read, :episodes, ["published_at <= ?", Time.zone.now] do |episode|
       episode.published_at <= Time.now.utc
     end
+    can :read, :comments
     can :access, :info # everybody can visit the about page
     can [:login, :read, :create, :login_with_github], :users
     if user # when sb logged in
       can [:edit, :update], :users, :id => user.id
-      can [:read, :create, :update, :destroy], :comments
+      can [:create, :update, :destroy], :comments
       can :logout, :users
       if user.github_username == "happypeter"||user.name == "happypeter" # the latter is for test on VM
         can :access, :all
