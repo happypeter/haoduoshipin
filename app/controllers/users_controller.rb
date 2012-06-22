bdc4e39dfd02f748195da2b68bfbed61269aa113
Peter Wang <happypeter1983@gmail.com>
2012-06-22 14:44:46 +0800

    redirect_to where I was, after github login #13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     # executed. that's the nature of || 
     @user = User.find_by_github_uid(omniauth["uid"]) || User.create_from_omniauth(omniauth)
     cookies.permanent[:token] = @user.token
-    redirect_to root_url, :notice => "Signed in successfully"
+    redirect_to_target_or_default root_url, :notice => "Signed in successfully"
   end
   def login_with_github_failure
     @reason = params[:message]  
