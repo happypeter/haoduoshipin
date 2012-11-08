b0252e0c34428afa8b1104296dea3987ee8b34a0
Peter Wang <happypeter1983@gmail.com>
2012-11-08 22:57:51 +0800

    At.js

--- Gemfile
+++ Gemfile
@@ -1,9 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.2.6'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
+gem "jquery-atwho-rails", "0.1.6"
 
 
 # Gems used only for assets and not required
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,16 @@
 //= require vendor/jquery.jplayer.min
 //= require bootstrap
 //= require editable_comment
+//= require jquery.atwho
 //= require_self
+
+var commenter = [];
+var commenter_exist= [];
+$('.comment_head a').each(function() {
+  if($.inArray($(this).text(), commenter_exist) < 0){
+    commenter.push($(this).text());
+    commenter_exist.push($(this).text());
+  }
+});
+$('textarea').atWho('@', {data: commenter});
 
