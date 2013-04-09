7805dac07dfc5ab157c8e709fab2165e019c1a76
Peter Wang <happypeter1983@gmail.com>
2013-04-09 16:07:14 +0800

    no more type seconds by hand fix #191

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,10 +2,26 @@ class Episode < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   has_many :taggings, :dependent => :destroy
   has_many :tags, :through => :taggings
-  validates_presence_of :seconds, :name, :description, :notes
+  validates_presence_of :name
 
   scope :recent, order('id DESC')
-  
+  after_create :set_seconds
+
+  def get_video_duration
+    result = `avconv -i http://media.happycasts.net/assets/episodes/videos/#{asset_name}.mov 2>&1`
+    if result =~ /Duration: ([\d][\d]:[\d][\d]:[\d][\d].[\d]+)/
+      return $1.to_s
+    end
+    return "avconv error"
+  end
+
+  def set_seconds
+    du = get_video_duration
+    a = du.split(':')
+    self.seconds = a[0].to_i*3600 + a[1].to_i*60 + a[2].to_i
+    self.save
+  end
+
   def asset_name
     [id.to_s.rjust(3, "0"), name].join("-")
   end
