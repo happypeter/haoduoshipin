0ce3fe74be34c99773e4f7f7293142790b057911
Peter Wang <happypeter1983@gmail.com>
2013-06-14 17:31:51 +0800

    :domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Happycasts::Application.config.session_store :cookie_store, :key => '_happycasts_session'
+Happycasts::Application.config.session_store :cookie_store, :key => '_happycasts_session', :domain => "happycasts.net"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
