828d97ad751c112965fe2340c01c9898f6a10b66
Peter Wang <happypeter1983@gmail.com>
2013-06-26 10:09:16 +0800

    Revert ":domain"
    This reverts commit 0ce3fe74be34c99773e4f7f7293142790b057911.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Happycasts::Application.config.session_store :cookie_store, :key => '_happycasts_session', :domain => "happycasts.net"
+Happycasts::Application.config.session_store :cookie_store, :key => '_happycasts_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
