21947c7f34b6188a8754f50042135d691684fb72
Peter Wang <happypeter1983@gmail.com>
2013-07-18 11:44:01 +0800

    rm app_config.yml, using settingslogic now

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'yaml'
-APP_CONFIG = YAML.load(File.read(File.expand_path('../app_config.yml', __FILE__)))
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Sign up at https://github.com/account/applications
-  provider :github, APP_CONFIG["github_id"], APP_CONFIG["github_secret"]
+  provider :github, Settings.github.id, Settings.github.secret
 end
