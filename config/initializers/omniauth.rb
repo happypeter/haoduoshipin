c893109b6e3ac0a30e4c24e6b58e97e31cd2b0c2
Peter Wang <happypeter1983@gmail.com>
2013-12-20 10:04:24 +0800

    github auth scope limited, fix #154

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Sign up at https://github.com/account/applications
-  provider :github, Settings.github.id, Settings.github.secret
+  provider :github, Settings.github.id, Settings.github.secret, scope: "user"
 end
