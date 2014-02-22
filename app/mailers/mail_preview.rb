36c39909690bf012dee39ba67b8b5d32101e03e0
Billie Zhang <billiecoder@gmail.com>
2014-02-22 22:32:47 +0800

    mail_view production env not work

--- app/mailers/mail_preview.rb
+++
@@ -1,7 +0,0 @@
-class MailPreview < MailView
-  def new_ep_release
-    user = Struct.new(:email, :name).new("tom@gmail.com", "Tom")
-    episode = Episode.last
-    HappyMailer.new_ep_release(user, episode.id)
-  end
-end
