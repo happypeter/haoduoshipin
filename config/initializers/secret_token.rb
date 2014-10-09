f12694cf97a106183eefb1ba280fb4f0b57ae10c
Billie Zhang <billiecoder@gmail.com>
2014-10-09 20:10:16 +0800

    modify the value of secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Happycasts::Application.config.secret_token = 'ae32a91e9a29e65f4510a96ea09535b4c12698369eb0032a32d5db78deb17414bc23e620bbdad5e6a078d94a8c13aef586841c6c145d0f478106b6b234c535d2'
-Happycasts::Application.config.secret_key_base = 'ae32a91e9a29e65f4510a96ea09535b4c12698369eb0032a32d5db78deb17414bc23e620bbdad5e6a078d94a8c13aef586841c6c145d0f478106b6b234c535d2'
+Happycasts::Application.config.secret_key_base = 'ssae32a91e9a29e65f4510a96ea09535b4c12698369eb0032a32d5db78deb17414bc23e620bbdad5e6a078d94a8c13aef586841c6c145d0f478106b6b234c535d2'
