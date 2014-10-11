cec8fc4e3a5b278bde545fa99997aa95622dcb1a
Billie Zhang <billiecoder@gmail.com>
2014-10-11 22:30:17 +0800

    convert markdown rightly #225

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,7 +35,8 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    #renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
     options = {
       autolink: true,
       no_intra_emphasis: true,
