46c99fcd573de2301c21cdbbcffd284168b59323
Billie Zhang <billiecoder@gmail.com>
2014-10-24 13:57:47 +0800

    i

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003072715) do
+ActiveRecord::Schema.define(version: 20141024055556) do
 
   create_table "comments", force: true do |t|
     t.integer  "episode_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20141003072715) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "episodes"
   end
 
   create_table "users", force: true do |t|
