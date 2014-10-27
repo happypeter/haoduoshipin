f58955cd4edd71ee4801f5754ce68ed9738f34c8
Peter Wang <happypeter1983@gmail.com>
2014-10-27 10:23:55 +0800

    add videojs font to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Happycasts::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
+  config.assets.precompile += %w( .eot .woff .ttf )
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
