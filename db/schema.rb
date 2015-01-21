e32345d62b6471600f7ce22e89fcf25e35bdd73c
Peter Wang <happypeter1983@gmail.com>
2015-01-20 20:55:33 -0500

    bundle on server

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125055231) do
+ActiveRecord::Schema.define(version: 20141125035608) do
 
   create_table "comments", force: true do |t|
     t.integer  "episode_id"
@@ -46,8 +46,8 @@ ActiveRecord::Schema.define(version: 20141125055231) do
     t.integer  "user_id"
     t.integer  "comment_id"
     t.boolean  "unread",     default: true
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "taggings", force: true do |t|
