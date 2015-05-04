80f6f9584bec51bd263ccd27500354ff9bc5e2d3
Billie Zhang <billiecoder@gmail.com>
2015-05-04 10:10:56 +0800

    modify url

--- app/mailers/happy_mailer.rb
+++ app/mailers/happy_mailer.rb
@@ -1,16 +1,16 @@
 class HappyMailer < ActionMailer::Base
-  default from: "no-reply@happycasts.net"
+  default from: "no-reply@haoduoshipin.com"
 
   def mail_to_all(uid, mailbody)
     @mailbody = mailbody
     @user = User.find(uid)
-    mail(:to => @user.email, :subject => "Happycasts")
+    mail(:to => @user.email, :subject => "haoduoshipin")
   end
 
   def new_ep_release(uid, episode_id)
     @episode = Episode.find(episode_id)
     @user = User.find(uid)
-    mail(:to => @user.email, :subject => "Happycasts:#{@episode.name}")
+    mail(:to => @user.email, :subject => "haoduoshipin:#{@episode.name}")
   end
 
   def password_reset(uid)
