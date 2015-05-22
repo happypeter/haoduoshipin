405f232b89e075bdc496b59ec1330e716917ce7c
Billie Zhang <billiecoder@gmail.com>
2015-05-22 09:54:17 +0800

    index episode after it was created

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -12,8 +12,10 @@ class Episode < ActiveRecord::Base
   validates_presence_of :name, :tag_names
   scope :recent, -> { order(id: :desc) }
 
-  after_create :set_ratio
   attr_accessible :name, :note, :published_at, :revision, :published, :description, :ratio, :seconds, :tag_names, :baidu
+
+  after_create :swiftype_index_episode
+  after_create :set_ratio
 
   def should_be_published?
     if self.published_at < Time.now
@@ -54,5 +56,21 @@ class Episode < ActiveRecord::Base
     # 以前是视频长宽比是最早 4:3 后来 16：9，现在时 16：10 了，未来如果变了，直接改这里就行
     self.ratio = 1.6 # for macbook pro
     self.save
+  end
+
+  def swiftype_index_episode
+    client = Swiftype::Client.new
+    url = "http://haoduoshipin.com/v/#{self.id}"
+    document = {
+      :external_id => self.id,
+      :fields => [{:name => 'title', :value => self.name, :type => 'string'},
+        {:name => 'description', :value => self.description, :type => 'text'},
+        {:name => 'created_at', :value => self.created_at.iso8601, :type => 'date'},
+        {:name => 'asset_name', :value => self.asset_name, :type => 'enum'},
+        {:name => 'note', :value => self.note, :type => 'text'},
+        {:name => 'url', :value => url, :type => 'enum'},
+        {:name => 'revision', :value => self.revision, :type => 'integer'}]
+    }
+    client.create_or_update_document('hdsp', 'episodes', document)
   end
 end
