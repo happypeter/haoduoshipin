15b2f0eb9a1c2cb250afb2d11a85271eb19413ae
Billie Zhang <billiecoder@gmail.com>
2016-02-04 15:36:58 +0800

    read data from disk

--- server/methods.js
+++ server/methods.js
@@ -1,12 +1,19 @@
+var fs = Npm.require('fs');
+var path = Npm.require('path');
+
 Meteor.methods({
   '/video/getPost': function(postId) {
-    var posts = Assets.getText('posts.json');
+    var dataPath = path.join(process.env.HOME, '/doc-haoduoshipin/posts.json');
+
+    var posts = fs.readFileSync(dataPath, 'utf8');
+
     if ( postId == "posts" ) {
       return posts;
     } else {
       var postId = parseInt(postId);
       var metaData = JSON.parse(posts)[postId - 1];
-      var postContent = Assets.getText('posts/' + postId + '.md');
+      var postPath = path.join(process.env.HOME, '/doc-haoduoshipin/posts/' + postId + '.md');
+      var postContent = fs.readFileSync(postPath, 'utf8');
       return {
         metaData: metaData,
         postContent: postContent
