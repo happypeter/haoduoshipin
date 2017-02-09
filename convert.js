8f71cefafe025685a7539da88703428f54f04aa2
Peter Wang <happypeter1983@gmail.com>
2017-02-09 21:23:38 +0800

    i

--- convert.js
+++ convert.js
@@ -122,3 +122,25 @@ if(!fs.existsSync(distCssDir)){
 }
 
 fs.createReadStream(`${cssDir}/main.css`).pipe(fs.createWriteStream(`${distCssDir}/main.css`));
+
+let searchFilePath = __dirname + '/src/search.html';
+let distSearchFilePath = distDir + '/search.html';
+fs.createReadStream(searchFilePath).pipe(fs.createWriteStream(distSearchFilePath));
+
+
+// start express sereer
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('dist'));
+// 用跟本文件平级的一个 public 夹作为静态文件的存放位置
+// 没有这一行，后面 sendFile 的 index.html 就找不到了。
+
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+app.listen(3000, function(err) {
+  console.log('Listening at http://localhost:3000');
+});
