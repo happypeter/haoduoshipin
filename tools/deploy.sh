f9a343532a9db72ca0aefe4c7451d79027da3912
Peter Wang <happypeter1983@gmail.com>
2017-02-11 10:23:08 +0800

    i

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,3 +1,4 @@
 # node sitemap.js
 scp ../dist/v/$1.html haoduoshipin.com:haoduo-dist/v/
 scp ../dist/index.html haoduoshipin.com:haoduo-dist/
+scp ../dist/css/main.css haoduoshipin.com:haoduo-dist/css/
