438f007bf3fadf76653aa91fa77771948c96069e
Peter Wang <happypeter1983@gmail.com>
2017-02-17 15:56:54 +0800

    i

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,5 +1,5 @@
 # node sitemap.js
-scp ../dist/v/$1.html haoduoshipin.com:haoduo-dist/v/
-scp ../dist/index.html haoduoshipin.com:haoduo-dist/
-scp ../dist/css/main.css haoduoshipin.com:haoduo-dist/css/
-scp ../dist/css/highlight.css haoduoshipin.com:haoduo-dist/css/
+scp ./dist/v/$1.html haoduoshipin.com:haoduo-dist/v/
+scp ./dist/index.html haoduoshipin.com:haoduo-dist/
+scp ./dist/css/main.css haoduoshipin.com:haoduo-dist/css/
+scp ./dist/css/highlight.css haoduoshipin.com:haoduo-dist/css/
