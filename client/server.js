dcb5d356fbb60aec07ef7bdf9acfab1a25fb2151
Peter Wang <happypeter1983@gmail.com>
2017-09-17 19:02:12 +0800

    set port to 3012

--- client/server.js
+++ client/server.js
@@ -24,9 +24,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(3012, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:3012')
   })
 })
 .catch((ex) => {
